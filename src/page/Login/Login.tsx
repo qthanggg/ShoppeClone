8c9a4a4a35ce17ca7eededf11af5516d73054fe6
ThangPHQ <quocthang2024@gmail.com>
2025-04-17 23:54:07 +0700

    validate login forrm

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import { rules } from '@/utils/rules'
+import { FormValues } from '@/utils/rules'
 export default function Login() {
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = useForm<FormValues>()
 
   const onsubmit = handleSubmit((data) => {
     console.log(data)
@@ -16,25 +18,25 @@ export default function Login() {
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onsubmit}>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onsubmit} noValidate>
               <div className='text-2xl'>Đăng nhập</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Email'
+                  {...register('email', rules.email)}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>{errors.email?.message}</div>
               </div>
               <div className='mt-3'>
                 <input
                   type='password'
-                  name='password'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Password'
+                  {...register('password', rules.password)}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>{errors.password?.message}</div>
               </div>
               <div className='mt-3'>
                 <button
