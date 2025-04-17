017e2391141e8dcd30b7efa3bc84cd494ece440e
ThangPHQ <quocthang2024@gmail.com>
2025-04-17 23:49:37 +0700

    validate register form with react-hook-form

---
+++ src/utils/rules.ts
@@ -0,0 +1,60 @@
+import type { RegisterOptions } from 'react-hook-form'
+
+export interface FormValues {
+  email: string
+  password: string
+  confirm_password: string
+}
+
+type Rules = {
+  [key in keyof FormValues]?: RegisterOptions<FormValues, key>
+}
+
+export const rules: Rules = {
+  email: {
+    required: {
+      value: true,
+      message: 'Email là bắt buộc'
+    },
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email không đúng định dạng'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    }
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  },
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Nhập lại password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  }
+}
